fdd7ab5b2b3318dc328aacb743c0a2d536321111
GQ <2321118172@qq.com>
2020-12-16 11:38:49 +0800

    2222

--- giao/app/src/main/java/com/example/giao/Activity/BottomMenuActivity.java
+++ giao/app/src/main/java/com/example/giao/Activity/BottomMenuActivity.java
@@ -7,12 +7,29 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
-public class BottomMenuActivity extends AppCompatActivity {
+public class BottomMenuActivity extends AppCompatActivity{
     private TextView mTextMessage;
+    private CountStepsFragment count_steps_fragment;
+    private StepRankFragment mm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_menu);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        count_steps_fragment = new CountStepsFragment();
+        mm = new StepRankFragment();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,10 +38,13 @@ public class BottomMenuActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    // reomoveFragment(count_steps_fragment())
+                    replaceFragment(count_steps_fragment);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    replaceFragment(mm);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
@@ -34,13 +54,28 @@ public class BottomMenuActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_menu2);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
+        transaction.replace(R.id.Fragment_Count_Stepss, fragment);
+        transaction.commit();
     }
+
+    private void reomoveFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
+        transaction.remove(fragment);
+        transaction.commit();
+    }
+
+//    private void addFragment(Fragment fragment){
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
+//        transaction.add(fragment)
+//        transaction.commit();
+//    }
+
 
 }

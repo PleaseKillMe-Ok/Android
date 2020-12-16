6fc0013879086f1ca6eddd66394ae48f5deb9211
syz247179876 <247179876@qq.com>
2020-12-16 16:16:46 +0800

    解决fragment跳转重叠，不显示的bug

--- giao/app/src/main/java/com/example/giao/Activity/BottomMenuActivity.java
+++ giao/app/src/main/java/com/example/giao/Activity/BottomMenuActivity.java
@@ -3,32 +3,39 @@ package com.example.giao.Activity;
 import android.os.Bundle;
 
 import com.example.giao.R;
+import com.example.giao.fragment.CountStepsFragment;
+import com.example.giao.fragment.StepRankFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 public class BottomMenuActivity extends AppCompatActivity{
     private TextView mTextMessage;
     private CountStepsFragment count_steps_fragment;
-    private StepRankFragment mm;
+    private StepRankFragment rank_steps_fragment;
+    private FragmentTransaction transaction;
+    private FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_menu);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navView = findViewById(R.id.nav_view);  // navView控件
+        mTextMessage = findViewById(R.id.message);  //测试移动
+        manager = getSupportFragmentManager();//获取FragmentManage的方式
+        transaction = manager.beginTransaction();//开启一个事务
+        // 设定navView的选择监听器
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // 默认选中第一个
         count_steps_fragment = new CountStepsFragment();
-        mm = new StepRankFragment();
+        transaction.replace(R.id.fragment_content, count_steps_fragment);
+        transaction.commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -36,17 +43,23 @@ public class BottomMenuActivity extends AppCompatActivity{
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            manager = getSupportFragmentManager();
+            transaction = manager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_home);
-                    // reomoveFragment(count_steps_fragment())
-                    replaceFragment(count_steps_fragment);
+                    hideFragment(transaction); // 清除旧的fragment
+                    count_steps_fragment = new CountStepsFragment();
+                    // 一次commit对于一个transaction只能提交一次，因此需要new一个新的transaction
+                    transaction.replace(R.id.fragment_content, count_steps_fragment).commit();
                     return true;
                 case R.id.navigation_dashboard:
+                    hideFragment(transaction); // 清除旧的fragment
                     mTextMessage.setText(R.string.title_dashboard);
-                    replaceFragment(mm);
+                    rank_steps_fragment = new StepRankFragment();
+                    transaction.replace(R.id.fragment_content, rank_steps_fragment).commit();
                     return true;
                 case R.id.navigation_notifications:
+                    hideFragment(transaction); // 清除旧的
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -56,26 +69,20 @@ public class BottomMenuActivity extends AppCompatActivity{
 
 
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
-        transaction.replace(R.id.Fragment_Count_Stepss, fragment);
-        transaction.commit();
-    }
+    /*
+     * 去除（隐藏）所有的Fragment
+     * */
+    private void hideFragment(FragmentTransaction transaction) {
+        if (count_steps_fragment != null) {
+            //transaction.hide(f1);  隐藏方法也可以实现同样的效果，不过我一般使用去除
+           transaction.remove(count_steps_fragment);
+        }
+        if (rank_steps_fragment != null) {
+            //transaction.hide(f2);
+          transaction.remove(rank_steps_fragment);
+        }
 
-    private void reomoveFragment(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
-        transaction.remove(fragment);
-        transaction.commit();
     }
-
-//    private void addFragment(Fragment fragment){
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
-//        transaction.add(fragment)
-//        transaction.commit();
-//    }
 
 
 }
--- giao/app/src/main/java/com/example/giao/fragment/CountStepsFragment.java
+++ giao/app/src/main/java/com/example/giao/fragment/CountStepsFragment.java
@@ -1,19 +1,23 @@
-package com.example.giao.Activity;
+package com.example.giao.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.giao.R;
 
+// Fragment 片段，设置初始内容
 public class CountStepsFragment extends Fragment {
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_count_steps, container, false);
+        //对视图中的控件进行初始化
         return view;
     }
 }
